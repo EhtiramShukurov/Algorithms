45f35698ba0331a5619897c834fd6d8f7f672143
EhtiramShukurov <116624192+EhtiramShukurov@users.noreply.github.com>
2022-10-29 21:30:43 +0400

    Added Problem_4.cs file

---
+++ Problem_4.cs
@@ -0,0 +1,36 @@
+﻿namespace Problem_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            switch (month)
+            {
+                case "February":
+                    Console.WriteLine(28);
+                    break;
+                case "April": 
+                case "June":
+                case "September":
+                case "November":
+                    Console.WriteLine(30);
+                    break;
+                case "January":
+                case "March":
+                case "May":
+                case "July":
+                case "August":
+                case "October":
+                case "December":
+                    Console.WriteLine(31);
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter a month");
+                    break;
+                
+            }
+        }
+    }
+}

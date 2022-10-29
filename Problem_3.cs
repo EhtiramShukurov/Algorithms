1e7807219c4fc2c443f10818c3728285fb47a91f
EhtiramShukurov <116624192+EhtiramShukurov@users.noreply.github.com>
2022-10-29 21:30:17 +0400

    Added Problem_3.cs file

---
+++ Problem_3.cs
@@ -0,0 +1,22 @@
+﻿namespace Problem_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = Convert.ToChar(Console.Read());
+            if (letter == 'a' || letter == 'A')
+            {
+                Console.WriteLine("Azerbaycan dili sechildi");
+            }
+            else if (letter == 'r' || letter == 'R')
+            {
+                Console.WriteLine("Rus dili sechildi");
+            }
+            else
+            {
+                Console.WriteLine("Yanlish sorghu");
+            }
+        }
+    }
+}

19e9539805130a472e3f730a099557553121974d
EhtiramShukurov <116624192+EhtiramShukurov@users.noreply.github.com>
2022-10-29 21:29:46 +0400

    Added Problem_2.cs file

---
+++ Problem_2.cs
@@ -0,0 +1,22 @@
+﻿namespace Problem_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int m, n, k;
+            m = number / 100;
+            n = number / 10 % 10;
+            k = number % 10;
+            if (m == n && n == k)
+            {
+                Console.WriteLine("Butun reqemler beraberdir");
+            }
+            else
+            {
+                Console.WriteLine("Butun reqemler beraber deyil");
+            }
+        }
+    }
+}

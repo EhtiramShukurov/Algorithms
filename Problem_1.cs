c90652fd3b3d9b590baccf5c1db79124b84e1162
EhtiramShukurov <116624192+EhtiramShukurov@users.noreply.github.com>
2022-10-29 21:29:00 +0400

    Added Problem_1.cs file

---
+++ Problem_1.cs
@@ -0,0 +1,16 @@
+﻿namespace Problem_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, a, b, c, d;
+            number = Convert.ToInt32(Console.ReadLine());
+            a = number / 1000;
+            b = number / 100 % 10;
+            c = number / 10 % 10;
+            d = number % 10;
+            Console.WriteLine(a + b + c + d);
+        }
+    }
+}
